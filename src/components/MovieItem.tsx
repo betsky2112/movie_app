f82a6004321887d19c2af099f1ae8485d481c9fc
betsky2112 <roberthussebastians@gmail.com>
2024-06-13 08:28:38 +0700

    Complete Home Screen

--- src/components/MovieItem.tsx
+++ src/components/MovieItem.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
-import { ImageBackground, Text, StyleSheet, View } from 'react-native'
+import {
+    ImageBackground,
+    Text,
+    StyleSheet,
+    View,
+    TouchableOpacity,
+} from 'react-native'
 import { FontAwesome } from '@expo/vector-icons'
 import { LinearGradient } from 'expo-linear-gradient'
 import type { MovieItemProps } from '../types/app'
+import { useNavigation, StackActions } from '@react-navigation/native'
 
 const MovieItem = ({ movie, size, coverType }: MovieItemProps): JSX.Element => {
+    const navigation = useNavigation()
+    const pushNavigation = StackActions.push('MovieDetail', { id: movie.id })
     return (
-        <View>
+        <TouchableOpacity onPress={() => navigation.dispatch(pushNavigation)}>
             <ImageBackground
                 resizeMode="cover"
                 style={[size, styles.backgroundImage]}
@@ -33,7 +42,7 @@ const MovieItem = ({ movie, size, coverType }: MovieItemProps): JSX.Element => {
                     </View>
                 </LinearGradient>
             </ImageBackground>
-        </View>
+        </TouchableOpacity>
     )
 }
 
--- src/screens/MovieDetail.tsx
+++ src/screens/MovieDetail.tsx
@@ -2,49 +2,20 @@ import { API_ACCESS_TOKEN, API_URL } from '@env'
 import React from 'react'
 import { View, Text, Button, StyleSheet } from 'react-native'
 
-const MovieDetail = ({ navigation }: any): any => {
-    const fetchData = (): void => {
-        if (API_URL == null || API_ACCESS_TOKEN.length == null) {
-            throw new Error('ENV not found')
-        }
-
-        const options = {
-            method: 'GET',
-            headers: {
-                accept: 'application/json',
-                Authorization: `Bearer ${API_ACCESS_TOKEN}`,
-            },
-        }
-
-        fetch(API_URL, options)
-            .then(async (response) => await response.json())
-            .then((response) => {
-                console.log(response)
-            })
-            .catch((err) => {
-                console.error(err)
-            })
-    }
+const MovieDetail = ({ route }: any): JSX.Element => {
+    const { id } = route.params
 
     return (
-        <View style={styles.container}>
-            <Text>Movie Detail Page</Text>
-            <Button
-                title="Fetch Data"
-                onPress={() => {
-                    fetchData()
-                }}
-            />
+        <View
+            style={{
+                display: 'flex',
+                alignItems: 'center',
+                marginTop: 32,
+            }}
+        >
+            <Text style={{ fontSize: 30 }}>Movie ID: {id}</Text>
         </View>
     )
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-})
 
 export default MovieDetail
